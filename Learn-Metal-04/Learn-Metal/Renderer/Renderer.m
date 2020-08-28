0ca72395e8ac8d8dbc66541ba96d0ddf64cea508
hjw <573986558@qq.com>
2020-08-28 18:20:06 +0800

    update texture

--- Learn-Metal-04/Learn-Metal/Renderer/Renderer.m
+++ Learn-Metal-04/Learn-Metal/Renderer/Renderer.m
@@ -7,6 +7,7 @@
 //
 
 #import "Renderer.h"
+#include "ShaderTypes.h"
 
 @interface Renderer ()
 {
@@ -15,6 +16,14 @@
     id<MTLRenderPipelineState> _renderPipelineState;
     
     id<MTLCommandQueue> _commandQueue;
+    
+    id<MTLBuffer> _vertexBuffer;
+    
+    NSInteger _numberVertices;
+    
+    id<MTLTexture> _texture;
+    
+    vector_int2 _viewportSize;
     
     MTKView *_mtkView;
 }
@@ -39,7 +48,7 @@
 }
 
 - (void)setupPipeline {
- 
+    
     id<MTLLibrary> defaultLibrary = [_device newDefaultLibrary];
     id<MTLFunction> vertexShader  = [defaultLibrary newFunctionWithName:@"vertexShader"];
     id<MTLFunction> fragmentShader  = [defaultLibrary newFunctionWithName:@"fragmentShader"];
@@ -58,19 +67,100 @@
 }
 
 - (void)setupVertex {
+    static const Vertex squardVertices[] = {
+        { {  250, -250 }, { 1.0f, 0.0f } },
+        { { -250, -250 }, { 0.0f, 0.0f } },
+        { { -250,  250 }, { 0.0f, 1.0f } },
+        
+        { {  250, -250 }, { 1.0f, 0.0f } },
+        { { -250,  250 }, { 0.0f, 1.0f } },
+        { {  250,  250 }, { 1.0f, 1.0f } },
+    };
+    _vertexBuffer = [_device newBufferWithBytes:squardVertices length:sizeof(squardVertices) options:MTLResourceStorageModeShared];
     
+    _numberVertices = sizeof(squardVertices) / sizeof(Vertex);
 }
 
 - (void)setupTexture {
+    UIImage *image = [UIImage imageNamed:@"wlop.png"];
     
+    MTLTextureDescriptor *textureDes = [[MTLTextureDescriptor alloc] init];
+    textureDes.pixelFormat = MTLPixelFormatRGBA8Unorm;
+    textureDes.width    = image.size.width;
+    textureDes.height   = image.size.height;
+    
+    _texture = [_device newTextureWithDescriptor:textureDes];
+   
+    MTLRegion region = {
+        {0, 0, 0},
+        {image.size.width, image.size.height, 1},
+    };
+    
+    Byte *imageBytes = [self loadImage:image];
+    NSAssert(imageBytes, @"imageBytes load failed");
+
+    [_texture replaceRegion:region
+                mipmapLevel:0
+                  withBytes:imageBytes
+                bytesPerRow:4 * image.size.width];
+    
+    free(imageBytes);
+    imageBytes = NULL;
 }
 
-- (void)drawInMTKView:(nonnull MTKView *)view {
+- (Byte *)loadImage:(UIImage *)image {
+    CGImageRef imageRef = image.CGImage;
+    NSAssert(imageRef, @"Image load failed");
     
+    size_t width = CGImageGetWidth(imageRef);
+    size_t height = CGImageGetHeight(imageRef);
+    
+    Byte *spriteData = (Byte*)calloc(width * height * 4, sizeof(Byte));
+    
+    CGContextRef contextRef = CGBitmapContextCreate(spriteData, width, height, 8, width * 4, CGImageGetColorSpace(imageRef), kCGImageAlphaPremultipliedLast);
+    
+    CGRect rect = CGRectMake(0, 0, width, height);
+    CGContextDrawImage(contextRef, rect, imageRef);
+    CGContextTranslateCTM(contextRef, 0, rect.size.height);
+    CGContextScaleCTM(contextRef, 1.0, -1.0);
+    CGContextDrawImage(contextRef, rect, imageRef);
+
+    CGContextRelease(contextRef);
+    CGImageRelease(imageRef);
+    
+    return spriteData;
 }
 
 - (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size {
-    
+    _viewportSize.x = size.width;
+    _viewportSize.y = size.height;
 }
 
+- (void)drawInMTKView:(nonnull MTKView *)view {
+    id<MTLCommandBuffer> commandBuffer = [_commandQueue commandBuffer];
+    commandBuffer.label = @"Sample Texture";
+    
+    MTLRenderPassDescriptor *renderPassDes = view.currentRenderPassDescriptor;
+    if (renderPassDes != nil) {
+        id<MTLRenderCommandEncoder> commandEncoder = [commandBuffer renderCommandEncoderWithDescriptor:renderPassDes];
+        commandEncoder.label = @"Command Encoder";
+        
+        [commandEncoder setViewport:(MTLViewport){0.0, 0.0, _viewportSize.x, _viewportSize.y, -1.0, 1.0}];
+        
+        [commandEncoder setRenderPipelineState:_renderPipelineState];
+        
+        [commandEncoder setVertexBytes:&_viewportSize length:sizeof(_viewportSize) atIndex:VertexInputIndexViewportSize];
+        
+        [commandEncoder setVertexBuffer:_vertexBuffer offset:0 atIndex:VertexInputIndexVertices];
+        
+        [commandEncoder setFragmentTexture:_texture atIndex:TextureIndexBaseColor];
+        
+        [commandEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:_numberVertices];
+        
+        [commandEncoder endEncoding];
+        
+        [commandBuffer presentDrawable:view.currentDrawable];
+    }
+    [commandBuffer commit];
+}
 @end
